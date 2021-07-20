2ecad0c8e46700d6a6a262fa31b524a7096f2488
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2021-07-20 18:49:09 +0530

    [hiren/mrinal] written a test calculating the perimeter of square with implementation

--- square.js
+++ square.js
@@ -2,8 +2,13 @@ class Square {
   constructor(sideSize) {
     this.sideSize = sideSize;
   }
+
   getArea() {
     return Math.pow(this.sideSize, 2);
+  }
+
+  getPerimeter() {
+    return this.sideSize * 4;
   }
 }
 
--- square.test.js
+++ square.test.js
@@ -8,4 +8,13 @@ describe('Square', () => {
       expect(result).toBe(16);
     });
   });
+
+  describe('getPerimeter()', () => {
+    it('should return 20 for square of side size of 5', () => {
+      let square = new Square(5);
+
+      const result = square.getPerimeter();
+      expect(result).toBe(20);
+    });
+  });
 });

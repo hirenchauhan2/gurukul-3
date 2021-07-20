dbbce84b032c3fa04be6f037fefc6ef29c0f79ec
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2021-07-20 18:49:09 +0530

    remove unnecessary code

--- rectangle.test.js
+++ rectangle.test.js
@@ -1,9 +1,5 @@
 const Rectangle = require('./rectangle');
 
-function compileAndroidCode() {
-  throw new Error('YYou are using the wrong JDK--');
-}
-
 describe('Rectangle', () => {
   test('should just run!', () => {
     expect(1).toBe(1);
